b555bed1291f20fc2f89e5baf4b29f81b1b75df9
nate <nate@c83a0d04-e8ae-47eb-aa4b-142be30de269>
2011-12-07 21:32:44 +0000

    Imported CutFile source
    git-svn-id: http://svn.nate.deepcreek.org.au/svn/tools/CutFile@16 c83a0d04-e8ae-47eb-aa4b-142be30de269

---
+++ Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace CutFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonInputBrowse = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonOutputBrowse = new System.Windows.Forms.Button();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBarStart = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDownStart = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBarEnd = new System.Windows.Forms.TrackBar();
+            this.numericUpDownEnd = new System.Windows.Forms.NumericUpDown();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.labelInputSize = new System.Windows.Forms.Label();
+            this.labelOutputSize = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarStart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarEnd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownEnd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonInputBrowse
+            // 
+            this.buttonInputBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonInputBrowse.Location = new System.Drawing.Point(423, 29);
+            this.buttonInputBrowse.Name = "buttonInputBrowse";
+            this.buttonInputBrowse.Size = new System.Drawing.Size(24, 23);
+            this.buttonInputBrowse.TabIndex = 3;
+            this.buttonInputBrowse.Text = "...";
+            this.buttonInputBrowse.UseVisualStyleBackColor = true;
+            this.buttonInputBrowse.Click += new System.EventHandler(this.buttonInputBrowse_Click);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxInput.Location = new System.Drawing.Point(15, 31);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(402, 20);
+            this.textBoxInput.TabIndex = 2;
+            this.textBoxInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxInput_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input";
+            // 
+            // buttonOutputBrowse
+            // 
+            this.buttonOutputBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOutputBrowse.Location = new System.Drawing.Point(423, 78);
+            this.buttonOutputBrowse.Name = "buttonOutputBrowse";
+            this.buttonOutputBrowse.Size = new System.Drawing.Size(24, 23);
+            this.buttonOutputBrowse.TabIndex = 7;
+            this.buttonOutputBrowse.Text = "...";
+            this.buttonOutputBrowse.UseVisualStyleBackColor = true;
+            this.buttonOutputBrowse.Click += new System.EventHandler(this.buttonOutputBrowse_Click);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxOutput.Location = new System.Drawing.Point(15, 80);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(402, 20);
+            this.textBoxOutput.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Output";
+            // 
+            // trackBarStart
+            // 
+            this.trackBarStart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBarStart.LargeChange = 50;
+            this.trackBarStart.Location = new System.Drawing.Point(15, 131);
+            this.trackBarStart.Maximum = 1000;
+            this.trackBarStart.Name = "trackBarStart";
+            this.trackBarStart.Size = new System.Drawing.Size(327, 40);
+            this.trackBarStart.TabIndex = 9;
+            this.trackBarStart.TickFrequency = 50;
+            this.trackBarStart.Scroll += new System.EventHandler(this.trackBarStart_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Start Position";
+            // 
+            // numericUpDownStart
+            // 
+            this.numericUpDownStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownStart.Location = new System.Drawing.Point(348, 131);
+            this.numericUpDownStart.Name = "numericUpDownStart";
+            this.numericUpDownStart.Size = new System.Drawing.Size(99, 20);
+            this.numericUpDownStart.TabIndex = 10;
+            this.numericUpDownStart.ValueChanged += new System.EventHandler(this.numericUpDownStart_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "End Position";
+            // 
+            // trackBarEnd
+            // 
+            this.trackBarEnd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBarEnd.LargeChange = 50;
+            this.trackBarEnd.Location = new System.Drawing.Point(15, 190);
+            this.trackBarEnd.Maximum = 1000;
+            this.trackBarEnd.Name = "trackBarEnd";
+            this.trackBarEnd.Size = new System.Drawing.Size(327, 40);
+            this.trackBarEnd.TabIndex = 12;
+            this.trackBarEnd.TickFrequency = 50;
+            this.trackBarEnd.Scroll += new System.EventHandler(this.trackBarEnd_Scroll);
+            // 
+            // numericUpDownEnd
+            // 
+            this.numericUpDownEnd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownEnd.Location = new System.Drawing.Point(348, 190);
+            this.numericUpDownEnd.Name = "numericUpDownEnd";
+            this.numericUpDownEnd.Size = new System.Drawing.Size(99, 20);
+            this.numericUpDownEnd.TabIndex = 13;
+            this.numericUpDownEnd.ValueChanged += new System.EventHandler(this.numericUpDownEnd_ValueChanged);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSave.Location = new System.Drawing.Point(291, 243);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 14;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonClose.Location = new System.Drawing.Point(372, 243);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 15;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // labelInputSize
+            // 
+            this.labelInputSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelInputSize.AutoSize = true;
+            this.labelInputSize.Location = new System.Drawing.Point(366, 15);
+            this.labelInputSize.Name = "labelInputSize";
+            this.labelInputSize.Size = new System.Drawing.Size(51, 13);
+            this.labelInputSize.TabIndex = 1;
+            this.labelInputSize.Text = "InputSize";
+            this.labelInputSize.Visible = false;
+            // 
+            // labelOutputSize
+            // 
+            this.labelOutputSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelOutputSize.AutoSize = true;
+            this.labelOutputSize.Location = new System.Drawing.Point(366, 62);
+            this.labelOutputSize.Name = "labelOutputSize";
+            this.labelOutputSize.Size = new System.Drawing.Size(59, 13);
+            this.labelOutputSize.TabIndex = 5;
+            this.labelOutputSize.Text = "OutputSize";
+            this.labelOutputSize.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 277);
+            this.Controls.Add(this.labelOutputSize);
+            this.Controls.Add(this.labelInputSize);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.numericUpDownEnd);
+            this.Controls.Add(this.numericUpDownStart);
+            this.Controls.Add(this.trackBarEnd);
+            this.Controls.Add(this.trackBarStart);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.buttonOutputBrowse);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.buttonInputBrowse);
+            this.Name = "Form1";
+            this.Text = "CutFile";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarStart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarEnd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownEnd)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonInputBrowse;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonOutputBrowse;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBarStart;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDownStart;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBarEnd;
+        private System.Windows.Forms.NumericUpDown numericUpDownEnd;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Label labelInputSize;
+        private System.Windows.Forms.Label labelOutputSize;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CutFile
+{
+    public partial class Form1 : Form
+    {
+        private string _fileName = "";
+        private long _fileLength = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonInputBrowse_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.FileName = textBoxInput.Text;
+
+            var result = dialog.ShowDialog(this);
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                bool setOutput = ((textBoxOutput.Text.Length == 0) || (textBoxOutput.Text == GetCutFilename(textBoxInput.Text)));
+
+                textBoxInput.Text = dialog.FileName;
+                if (setOutput)
+                    textBoxOutput.Text = GetCutFilename(textBoxInput.Text);
+
+                LoadFile(dialog.FileName);
+            }
+        }
+        private void textBoxInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                bool setOutput = ((textBoxOutput.Text.Length == 0) || (textBoxOutput.Text == GetCutFilename(_fileName)));
+
+                if (setOutput)
+                    textBoxOutput.Text = GetCutFilename(textBoxInput.Text);
+
+                LoadFile(textBoxInput.Text);
+            }
+        }
+
+        private void buttonOutputBrowse_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.FileName = textBoxOutput.Text;
+
+            var result = dialog.ShowDialog(this);
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                textBoxOutput.Text = dialog.FileName;
+                LoadFile(dialog.FileName);
+            }
+        }
+
+        private string GetCutFilename(string fileName)
+        {
+            return Path.GetDirectoryName(fileName) + @"\" + Path.GetFileNameWithoutExtension(fileName) + "_cut" + Path.GetExtension(fileName);
+        }
+
+        public string GetNiceSize(long size)
+        {
+            double curr = 1.0;
+            long next = 1024;
+
+            if (size < next)
+                return size + " bytes";
+
+            curr = next;
+            next *= 1024;
+            if (size < next)
+                return (size / curr).ToString("0.000") + " KB";
+
+            curr = next;
+            next *= 1024;
+            if (size < next)
+                return (size / curr).ToString("0.000") + " MB";
+
+            curr = next;
+            next *= 1024;
+            if (size < next)
+                return (size / curr).ToString("0.000") + " GB";
+
+            curr = next;
+            next *= 1024;
+            if (size < next)
+                return (size / curr).ToString("0.000") + " TB";
+
+            return (size / curr).ToString("0.000") + " TB";
+        }
+
+        private void LoadFile(string fileName)
+        {
+            var fileInfo = new FileInfo(fileName);
+            _fileName = fileName;
+            _fileLength = fileInfo.Length;
+
+            numericUpDownStart.Maximum = _fileLength;
+            numericUpDownEnd.Maximum = _fileLength;
+            numericUpDownStart.Value = 0;
+            numericUpDownEnd.Value = _fileLength;
+            trackBarStart.Value = 0;
+            trackBarEnd.Value = trackBarEnd.Maximum;
+
+            labelInputSize.Text = GetNiceSize(_fileLength);
+            labelInputSize.Visible = true;
+            labelOutputSize.Visible = true;
+        }
+
+        private void trackBarStart_Scroll(object sender, EventArgs e)
+        {
+            numericUpDownStart.Value = (long)(_fileLength * (trackBarStart.Value / 1000.0));
+        }
+
+        private void trackBarEnd_Scroll(object sender, EventArgs e)
+        {
+            numericUpDownEnd.Value = (long)(_fileLength * (trackBarEnd.Value / 1000.0));
+        }
+
+        private void numericUpDownStart_ValueChanged(object sender, EventArgs e)
+        {
+            trackBarStart.Value = (int)((numericUpDownStart.Value / _fileLength) * 1000);
+            if (numericUpDownEnd.Value < numericUpDownStart.Value)
+                numericUpDownEnd.Value = numericUpDownStart.Value;
+            UpdateSize();
+        }
+
+        private void numericUpDownEnd_ValueChanged(object sender, EventArgs e)
+        {
+            trackBarEnd.Value = (int)((numericUpDownEnd.Value / _fileLength) * 1000);
+            if (numericUpDownEnd.Value < numericUpDownStart.Value)
+                numericUpDownStart.Value = numericUpDownEnd.Value;
+            UpdateSize();
+        }
+
+        private void UpdateSize()
+        {
+            labelOutputSize.Text = GetNiceSize((long)(numericUpDownEnd.Value - numericUpDownStart.Value));
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            this.Enabled = false;
+
+            long startPos = (long)numericUpDownStart.Value;
+            long endPos = (long)numericUpDownEnd.Value;
+
+            using (var input = File.OpenRead(_fileName))
+            {
+                input.Seek(startPos, SeekOrigin.Begin);
+
+                using (var output = File.OpenWrite(textBoxOutput.Text))
+                {
+                    byte[] buffer = new byte[65536];
+                    long bytesLeft = endPos - startPos;
+
+                    while (bytesLeft > 0)
+                    {
+                        int bytesToRead = buffer.Length;
+                        if (bytesLeft < buffer.Length)
+                            bytesToRead = (int)bytesLeft;
+                        int bytesRead = input.Read(buffer, 0, bytesToRead);
+                        if (bytesRead <= 0)
+                            break;
+                        output.Write(buffer, 0, bytesRead);
+                        bytesLeft -= bytesRead;
+                    }
+                }
+            }
+
+            this.Enabled = true;
+            buttonClose.Focus();
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
